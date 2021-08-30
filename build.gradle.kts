6fe8a4fa6e771b9e57df39ae98d11281cf892209
Blad3Mak3r <blademaker.live@gmail.com>
2021-08-30 18:54:01 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "tv.blademaker"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
